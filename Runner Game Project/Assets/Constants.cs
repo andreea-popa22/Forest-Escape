4c9479f9898305b73c5967e23cda810eb2ecc09c
Bara C. Tudor <vector2025@gmail.com>
2021-11-29 23:06:07 +0200

    add Lane constants

---
+++ Runner Game Project/Assets/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Lane
+{
+    public const float Left = Middle - Distance;
+    public const float Middle = 0;
+    public const float Right = Middle + Distance;
+
+    public const float Distance = 2;
+}
+
