43ad72c5c8926841c5245f1096e5be54ce1c1cce
Bara C. Tudor <vector2025@gmail.com>
2022-01-13 20:36:26 +0200

    add UI for start, pause, end of game

---
+++ Runner Game Project/Assets/Scripts/MainUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    public GameObject menu;
+    public Button startButton;
+    public PlayerController pc;
+    private InputField inputName;
+
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = GameObject.Find("Start Canvas");
+        
+        startButton = GameObject.Find("Start").GetComponent<Button>();
+        startButton.onClick.AddListener(StartGame);
+        
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+        
+        inputName = GameObject.Find("Name Textbox").GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return)) { SubmitName(inputName.text); }
+        
+    }
+
+    void SubmitName(string arg0)
+    {
+        Debug.Log(arg0);
+    }
+    
+    void StartGame()
+    {
+        Debug.Log("yes");
+        menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+        pc.enabled = !pc.enabled;
+    }
+    
+}
