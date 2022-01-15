e2382cb5bcf67f4d9881b03060fbfc3fc5c2d078
Bara C. Tudor <vector2025@gmail.com>
2022-01-15 03:48:27 +0200

    incarca obstacolele care vin

--- Runner Game Project/Assets/Scripts/DeactivateObstacles.cs
+++ Runner Game Project/Assets/Scripts/DeactivateObstacles.cs
@@ -7,51 +7,59 @@ public class DeactivateObstacles : MonoBehaviour
     private GameObject generatedObtacles;
     private GameObject manualObstacles;
     public int distanceToDeactivate = 10;
-    private int distancecToActivate = 100;
+    private int distancecToActivate = 150;
 
     private float timePassed = 0;
 
     void Start()
     {
         player = GameObject.Find("Player");
-        generatedObtacles = GameObject.Find("Obstacles");
-        manualObstacles = GameObject.Find("Obstacles-Manual");
+        //generatedObtacles = GameObject.Find("Obstacles");
         //foreach (var trans in generatedObtacles.transform.GetComponentsInChildren<Transform>())
-        //    trans.gameObject.SetActive(false);
-        //foreach (var trans in manualObstacles.transform.GetComponentsInChildren<Transform>())
-        //    trans.gameObject.SetActive(false);
+        //    if(trans != generatedObtacles.transform)
+        //        trans.gameObject.SetActive(false);
+        
+        manualObstacles = GameObject.Find("Obstacles-Manual");
+        foreach (var trans in manualObstacles.transform.GetComponentsInChildren<Transform>())
+            if(trans != manualObstacles.transform)
+                trans.gameObject.SetActive(false);
+        Check();
     }
 
     void FixedUpdate()
     {
         // ruleaza doar de 4 ori pe secunda
         timePassed += Time.fixedDeltaTime;
-        if (timePassed > 0.5)
+        if (timePassed > 1)
         {
-            timePassed -= 0.5f;
+            timePassed -= 1;
+            Check();
+        }
+    }
 
-            float playerZ = player.transform.position.z;
-            // dezactiveaza obstacolele daca sunt in spatele player-ului
-            foreach (var trans in generatedObtacles.transform.GetComponentsInChildren<Transform>(includeInactive: false))
-            {
-                if (trans != generatedObtacles.transform)
-                {
-                    if ((playerZ - trans.position.z) > distanceToDeactivate)
-                        trans.gameObject.SetActive(false);
-                    //if (trans.position.z > playerZ && trans.position.z - playerZ < distancecToActivate)
-                    //    trans.gameObject.SetActive(true);
-                }
-            }
-            foreach (var trans in manualObstacles.transform.GetComponentsInChildren<Transform>(includeInactive: false))
-            {
-                if (trans != manualObstacles.transform) 
-                { 
-                    if ((playerZ - trans.position.z) > distanceToDeactivate)
-                        trans.gameObject.SetActive(false);
-                    //if (trans.position.z > playerZ && trans.position.z - playerZ < distancecToActivate)
-                    //    trans.gameObject.SetActive(true);
-                }
+    void Check()
+    {
+        float playerZ = player.transform.position.z;
+        // dezactiveaza obstacolele daca sunt in spatele player-ului, activeaza pe cele care intra in vizor
+        foreach (var trans in manualObstacles.transform.GetComponentsInChildren<Transform>(includeInactive: true))
+        {
+            if (trans != manualObstacles.transform) 
+            { 
+                if (playerZ < trans.position.z && (playerZ - trans.position.z) > distanceToDeactivate)
+                    trans.gameObject.SetActive(false);
+                if (trans.position.z > playerZ && (trans.position.z - playerZ) < distancecToActivate)
+                    trans.gameObject.SetActive(true);
             }
         }
+        //foreach (var trans in generatedObtacles.transform.GetComponentsInChildren<Transform>(includeInactive: false))
+        //{
+        //    if (trans != generatedObtacles.transform)
+        //    {
+        //        if (playerZ < trans.position.z && (playerZ - trans.position.z) > distanceToDeactivate)
+        //            trans.gameObject.SetActive(false);
+        //        if (trans.position.z > playerZ && (trans.position.z - playerZ) < distancecToActivate)
+        //            trans.gameObject.SetActive(true);
+        //    }
+        //}
     }
 }
